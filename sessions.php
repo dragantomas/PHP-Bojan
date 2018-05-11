ae186a88f0d840567af5c757ce04cdb0e0d96599
Dragan Tomovski <32159088+dragantomas@users.noreply.github.com>
2018-05-11 13:50:31 +0200

    semos 1350 110518

---
+++ sessions.php
@@ -0,0 +1,19 @@
+<?php 
+session_start();
+?>
+
+<form action="<?=$_SERVER['PHP_SELF'];?>" method="post">
+	<input type="text" name="ime">
+	<button type="submit">Keep!</button>
+</form>
+
+<?php
+	if(isset($_POST['ime'])){
+		$_SESSION['ime'] = $_POST['ime'];
+	}
+?>
+
+<?php if(isset($_SESSION['ime'])){ ?>
+<h3><?=$_SESSION['ime'];?></h3>
+
+<?php } ?>
